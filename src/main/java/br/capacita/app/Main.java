67e26dcb7c06597e3f48dde865b7e9d58dcf23ca
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2026-02-09 23:19:18 -0300

    atualização da main

--- src/main/java/br/capacita/app/Main.java
+++ src/main/java/br/capacita/app/Main.java
@@ -4,7 +4,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner teclado = new Scanner(System.in);
+
         System.out.println("Bem-vindo ao Sistema de Empréstimo de Livros");
+
+        int opcao = 0;
+
+        while (opcao != 7) {
+            System.out.printf(
+                    "\nEsolha o que deseja fazer: "
+                    + "\n1 - Cadastar Livro"
+                    + "\n2 - Cadatrar Aluno"
+                    + "\n3 - Cadatrar Professor"
+                    + "\n4 - Pesquisar Livro"
+                    + "\n5 - Emprestar Livro"
+                    + "\n6 - Desvolver Livro"
+                    + "\n7 - Sair\n"
+            );
+
+            System.out.print("Digite sua opção >>> ");
+            opcao = teclado.nextInt();
+            if (opcao <= 0 || opcao > 7) {
+                System.out.println("Opção Inválida, tente novamente");
+            }
+
+            switch (opcao) {
+                case 1:
+                    break;
+                case 2:
+                    break;case 2:
+                    break;
+            }
+        }
     }
 }
