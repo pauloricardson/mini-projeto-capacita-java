28991269f8db81b9184386a0f0846019ca36d974
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2026-02-09 23:20:38 -0300

    atualização da main

--- src/main/java/br/capacita/app/Main.java
+++ src/main/java/br/capacita/app/Main.java
@@ -31,9 +31,17 @@ public class Main {
 
             switch (opcao) {
                 case 1:
+                    System.out.println("teste");
                     break;
                 case 2:
-                    break;case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
                     break;
             }
         }
