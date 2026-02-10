9299a71d702c5675776fbd3647f6584d37eef284
Paulo Ricardson Silva Costa <pauloricardsonsilvacosta@gmail.com>
2026-02-09 23:04:03 -0300

    Construção da classe usuario, aluno e professor

--- src/main/java/br/capacita/app/Main.java
+++ src/main/java/br/capacita/app/Main.java
@@ -1,7 +1,10 @@
 package br.capacita.app;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Bem-vindo ao Sistema de Empréstimo de Livros");
     }
 }
